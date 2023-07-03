d42c04a823b104e6ba0c89b7d47a9db5a547d934
Mostafa Razavi <mostafa@sepent.com>
2023-07-03 18:34:31 +0200

    Add an assembler and a new runner
    assemble.py can convert an SECD code file in sexpr form to binary form.
    The binary form can then be run by the new runner (ng.py).
    
    Notice that the new runner is actually a little slower than the old one,
    since the old runner pre-parser its input (literal integers for example
    are only parsed once). The new runner however has the potential for
    adding some features that might be difficult to add to the old one.

---
+++ assemble.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from sexpr import read, ParseError, Symbol
+
+
+class AssembleError(Exception):
+    pass
+
+
+def assemble(expr, start_offset: int) -> bytes:
+    if not isinstance(expr, list):
+        raise AssembleError('Input not a list')
+
+    i = 0
+    code = b''
+    while i < len(expr):
+        instr = expr[i]
+        i += 1
+
+        if not isinstance(instr, Symbol):
+            raise AssembleError(f'Invalid instruction: {instr}')
+
+        instr = instr.name
+        single_byte_instrs = {
+            'nil': 0x01,
+            'cons': 0x02,
+            'join': 0x03,
+            'ap': 0x04,
+            'ret': 0x05,
+            'printn': 0x06,
+            'printc': 0x07,
+            'halt': 0x08,
+            'add': 0x09,
+            'sub': 0x0a,
+            'lt': 0x0b,
+            'dum': 0x0c,
+            'rap': 0x0d,
+        }
+        opcode = single_byte_instrs.get(instr)
+        if opcode is not None:
+            code += bytes([opcode])
+        elif instr == 'ldc':
+            value = expr[i]
+            i += 1
+            if not isinstance(value, int):
+                raise AssembleError(f'Invalid argument for ldc: {value}')
+            code += bytes([0x20])
+            code += value.to_bytes(length=4, byteorder='big', signed=True)
+        elif instr == 'ld':
+            if not isinstance(expr[i], list) or \
+               len(expr[i]) != 2 or \
+               not isinstance(expr[i][0], int) or \
+               not isinstance(expr[i][1], int):
+                raise AssembleError(f'Invalid argument for ld: {expr[i]}')
+            frame, index = expr[i]
+            i += 1
+            code += bytes([0x21])
+            code += frame.to_bytes(length=2, byteorder='big', signed=False)
+            code += index.to_bytes(length=2, byteorder='big', signed=False)
+        elif instr == 'sel':
+            if not isinstance(expr[i], list) or not isinstance(expr[i + 1], list):
+                raise AssembleError(f'Invalid argument for sel: {expr[j]}')
+
+            code += bytes([0x22])
+
+            true_body = assemble(expr[i], start_offset + 8)
+            false_body = assemble(expr[i + 1], start_offset + len(code) + 8 + len(true_body))
+
+            code += len(true_body).to_bytes(length=4, byteorder='big', signed=False)
+            code += len(false_body).to_bytes(length=4, byteorder='big', signed=False)
+            code += true_body
+            code += false_body
+
+            i += 2
+        elif instr == 'ldf':
+            if not isinstance(expr[i], list):
+                raise AssembleError(f'Invalid argument for ldf: {expr[i]}')
+            body = expr[i]
+            i += 1
+            code += bytes([0x23])
+            body_code = assemble(body, start_offset + len(code) + 4)
+            code += len(body_code).to_bytes(length=4, byteorder='big', signed=False)
+            code += body_code
+
+    return code
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Assemble SECD instructions written in sexpr form '
+        'into binary machine code.')
+
+    parser.add_argument(
+        'input', default='-', nargs='?',
+        help='Input file. Stdin is used if not specified or a dash (-) '
+        'is passed instead. Defaults to reading from stdin.')
+
+    args = parser.parse_args()
+
+    if args.input == '-':
+        text = sys.stdin.read()
+    else:
+        with open(args.input) as f:
+            text = f.read()
+
+    try:
+        expr = read(text)
+    except ParseError as e:
+        print(f'Parse error: {e}')
+        sys.exit(1)
+
+    try:
+        code = assemble(expr, 0)
+    except AssembleError as e:
+        print(f'Assemble error: {e}')
+        sys.exit(1)
+
+    sys.stdout.buffer.write(code)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ng.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+
+class RunError(Exception):
+    pass
+
+
+class Secd:
+    def __init__(self, code):
+        self.code = code
+
+        self.s = []
+        self.e = []
+        self.c = 0
+        self.d = []
+        self.halt_code = None
+        self.dummy_frame = object()
+        self.debug = False
+
+    def run(self):
+        funcs = {
+            0x01: self.run_nil,
+            0x02: self.run_cons,
+            0x03: self.run_join,
+            0x04: self.run_ap,
+            0x05: self.run_ret,
+            0x06: self.run_printn,
+            0x07: self.run_printc,
+            0x08: self.run_halt,
+            0x09: self.run_add,
+            0x0a: self.run_sub,
+            0x0b: self.run_lt,
+            0x0c: self.run_dum,
+            0x0d: self.run_rap,
+            0x20: self.run_ldc,
+            0x21: self.run_ld,
+            0x22: self.run_sel,
+            0x23: self.run_ldf,
+        }
+        code_len = len(self.code)
+        while self.c < code_len and self.halt_code is None:
+            opcode = self.code[self.c]
+            self.c += 1
+
+            try:
+                func = funcs[opcode]
+            except KeyError:
+                raise RunError(f'Invalid op-code: {opcode}')
+
+            func()
+
+    def run_nil(self):
+        self.s.append([])
+        if self.debug: print('nil')
+
+    def run_cons(self):
+        v = self.s.pop()
+        l = self.s.pop()
+        self.s.append(l + [v])
+        if self.debug: print(f'cons {v} onto {l}')
+
+    def run_ldc(self):
+        value = self.code[self.c:self.c+4]
+        self.c += 4
+        value = int.from_bytes(value, byteorder='big', signed=True)
+        self.s.append(value)
+        if self.debug: print(f'ldc {value}')
+
+    def run_ld(self):
+        frame_idx = self.code[self.c:self.c+2]
+        index = self.code[self.c+2:self.c+4]
+        self.c += 4
+
+        frame_idx = int.from_bytes(frame_idx, byteorder='big', signed=False)
+        index = int.from_bytes(index, byteorder='big', signed=False)
+
+        frame = self.e[frame_idx]
+        value = frame[index]
+        self.s.append(value)
+        if self.debug: print(f'ld [{frame_idx}, {index}] -- frame={frame} value={value}')
+
+    def run_sel(self):
+        true_len = self.code[self.c:self.c+4]
+        true_len = int.from_bytes(true_len, byteorder='big', signed=False)
+        self.c += 4
+
+        false_len = self.code[self.c:self.c+4]
+        false_len = int.from_bytes(false_len, byteorder='big', signed=False)
+        self.c += 4
+
+        cond = self.s.pop()
+        self.d.append(self.c + true_len + false_len)
+        if cond == 0:
+            self.c += true_len
+
+        if self.debug: print(f'sel cond={cond}')
+
+    def run_join(self):
+        self.c = self.d.pop()
+        if self.debug: print(f'join')
+
+    def run_ldf(self):
+        body_size = self.code[self.c:self.c+4]
+        body_size = int.from_bytes(body_size, byteorder='big', signed=False)
+        self.c += 4
+        closure = (self.c, self.e)
+        self.s.append(closure)
+        self.c += body_size
+        if self.debug: print(f'ldf body_size={body_size}')
+
+    def run_ap(self):
+        closure = self.s.pop()
+        args = self.s.pop()
+        self.d.append((self.s, self.e, self.c))
+        new_c, new_e = closure
+        if self.debug: print(f'ap {self.c} => {new_c}')
+        self.s, self.e, self.c = [], [args] + new_e, new_c
+
+    def run_ret(self):
+        if len(self.s) > 0:
+            retval = self.s.pop()
+        else:
+            retval = [] # nil
+        self.s, self.e, self.c = self.d.pop()
+        self.s.append(retval)
+        if self.debug: print(f'ret retval={retval}')
+
+    def run_printn(self):
+        n = self.s.pop()
+        if self.debug: print(f'printn {n}')
+        print(n)
+
+    def run_printc(self):
+        n = self.s.pop()
+        if self.debug: print(f'printc {n}')
+        print(chr(n), end='')
+
+    def run_halt(self):
+        self.halt_code = self.s.pop()
+        if not isinstance(self.halt_code, int):
+            raise RunError(f'Non-numeric halt code: {self.halt_code}')
+        if self.debug: print(f'halt {self.halt_code}')
+
+    def run_add(self):
+        arg1 = self.s.pop()
+        arg2 = self.s.pop()
+        if not isinstance(arg1, int) or not isinstance(arg2, int):
+            raise RunError('"add" arguments must be integers.')
+        self.s.append(arg2 + arg1)
+        if self.debug: print(f'add {arg2} + {arg1}')
+
+    def run_sub(self):
+        arg1 = self.s.pop()
+        arg2 = self.s.pop()
+        if not isinstance(arg1, int) or not isinstance(arg2, int):
+            raise RunError('"lt" arguments must be integers.')
+        self.s.append(arg2 - arg1)
+        if self.debug: print(f'sub {arg2} - {arg1}')
+
+    def run_lt(self):
+        arg1 = self.s.pop()
+        arg2 = self.s.pop()
+        if not isinstance(arg1, int) or not isinstance(arg2, int):
+            raise RunError('"lt" arguments must be integers.')
+        self.s.append(1 if arg2 < arg1 else 0)
+        if self.debug: print(f'lt {arg2} < {arg1}')
+
+    def run_dum(self):
+        self.e = [self.dummy_frame] + self.e
+        if self.debug: print(f'dum')
+
+    def run_rap(self):
+        closure = self.s.pop()
+        args = self.s.pop()
+        self.d.append((self.s, self.e, self.c))
+        new_c, new_e = closure
+        if new_e[0] != self.dummy_frame:
+            raise RunError('No dummy frame.')
+
+        # replace dummy frame with actual frame
+        new_e[0] = args
+
+        if self.debug: print(f'rap {self.c} => {new_c}')
+        self.s, self.e, self.c = [], new_e, new_c
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Run binary SECD instructions.')
+
+    parser.add_argument(
+        'input', default='-', nargs='?',
+        help='Input file. Stdin is used if not specified or a dash (-) '
+        'is passed instead. Defaults to reading from stdin.')
+
+    args = parser.parse_args()
+
+    if args.input == '-':
+        code = sys.stdin.buffer.read()
+    else:
+        with open(args.input, 'rb') as f:
+            code = f.read()
+
+    m = Secd(code)
+    # m.debug = True
+    try:
+        m.run()
+    except RunError as e:
+        print(f'Run error: {e}')
+        sys.exit(1)
+
+    if m.halt_code is None:
+        print('Code exhausted.')
+    else:
+        print('Machine halted with code:', m.halt_code)
+
+
+if __name__ == '__main__':
+    main()
