d07f66d7285e48e391007560fbf13a9dcf9f540e
Mostafa Razavi <mostafa@sepent.com>
2023-07-11 20:35:28 +0200

    Improve test script

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
+import argparse
 from assemble import assemble
-from compile import compile_form, compile_toplevel
-from machinetypes import Bool
+from compile import assoc, compile_form, compile_toplevel
+from machinetypes import Symbol
 from read import print_sexpr, read
-from secd import RunError, Secd
+from secd import RunError, Secd, UserError
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Run test suite.')
+
+    parser.add_argument(
+        '--stop-on-error', '-x', action='store_true', default=False,
+        help='Stop on first error.')
+
+    args = parser.parse_args()
+
     with open('stdlib.lisp') as f:
         text = f.read()
 
@@ -24,6 +33,8 @@ def main():
             break
         test_exprs.append(expr)
 
+    errors = []
+    fails = []
     for expr in test_exprs:
         expr_asm = compile_form(expr, [])
         asm = lib_asm + expr_asm
@@ -31,17 +42,48 @@ def main():
         machine = Secd(code)
         try:
             machine.run()
+        except UserError:
+            err = machine.s[-1]
+            err_type = assoc(Symbol(':type'), err)
+            msg = f'User error of type {err_type} during macro expansion'
+            err_msg = assoc(Symbol(':msg'), err)
+            if err_msg is not None:
+                msg += f': {err_msg}'
+            errors.append((expr, msg))
+            print('E', end='', flush=True)
         except RunError as e:
-            print(f'error: {e}')
+            errors.append((expr, str(e)))
+            print('E', end='', flush=True)
         else:
             result = machine.s[-1]
             if result:
-                print('success')
+                print('.', end='', flush=True)
             else:
-                print('failure:')
-                print('    ', end='')
-                print_sexpr(expr)
+                print('F', end='', flush=True)
+                fails.append(expr)
 
+    print()
+    if fails:
+        print()
+        print('Failed test case(s):')
+        for expr in fails:
+            print('    ', end='')
+            print_sexpr(expr)
+
+    if errors:
+        print()
+        print('Error(s):')
+        for expr, exception in errors:
+            if isinstance(exception, UserError):
+                msg = xx
+            else:
+                msg = str(exception)
+            print(f'    {msg}: ', end='')
+            print_sexpr(expr)
+
+    if fails and errors:
+        print()
+        print('Failed.')
 
 if __name__ == '__main__':
     main()
