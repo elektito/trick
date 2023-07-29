791547af47e6c13cd13b861120430bcf0878a2d1
Mostafa Razavi <mostafa@sepent.com>
2023-07-29 22:50:50 +0200

    Refactor printer

--- print.py
+++ print.py
@@ -52,7 +52,10 @@ class Printer:
 
     def _print(self, obj):
         s = ''
-        if obj in self._shared and obj not in self._labels:
+        if obj in self._shared:
+            n = self._labels.get(obj)
+            if n is not None:
+                return f'#{n}#'
             n = self._assign_label(obj)
             s = f'#{n}='
 
@@ -65,17 +68,14 @@ class Printer:
 
         return s
 
-    def _print_pair(self, pair, *, at_start=True):
-        s = ''
-
+    def _print_pair(self, pair):
         special = {
             Symbol('quote'): "'",
             Symbol('quasiquote'): '`',
             Symbol('unquote'): ',',
             Symbol('unquote-splicing'): ',@',
         }
-        if at_start and \
-           pair.car in special and \
+        if pair.car in special and \
            isinstance(pair.cdr, Pair) and \
            isinstance(pair.cdr.cdr, Nil):
             s = special[pair.car]
@@ -89,61 +89,38 @@ class Printer:
                 s += self._print(pair.cdr.car)
             return s
 
-        if at_start:
-            s += '('
-        else:
-            s += ' '
-
-        if pair.car in self._shared:
-            n = self._labels.get(pair.car)
-            if n is None:
-                n = self._assign_label(pair.car)
-                s += f'#{n}='
-                s += self._print(pair.car)
-            else:
-                s += f'#{n}#'
-        else:
+        s = '('
+        while isinstance(pair, Pair):
+            if s != '(':
+                s += ' '
             s += self._print(pair.car)
 
-        if isinstance(pair.cdr, Nil):
-            s += ')'
-        elif isinstance(pair.cdr, Pair):
             if pair.cdr in self._shared:
                 n = self._labels.get(pair.cdr)
                 if n is None:
-                    n = self._assign_label(pair.cdr)
-                    s += f' . #{n}='
-                    s += self._print_pair(pair.cdr, at_start=True)
-                    s += ')'
+                    s += f' . '
+                    s += self._print(pair.cdr)
                 else:
-                    s += f' . #{n}#)'
-            else:
-                s += self._print_pair(pair.cdr, at_start=False)
-        else:
-            s += ' . '
-            s += self._print(pair.cdr)
-            s += ')'
+                    s += f' . #{n}#'
+                pair = Nil()
+                break
 
+            pair = pair.cdr
+
+        if not isinstance(pair, Nil):
+            s += ' . '
+            s += self._print(pair)
+
+        s += ')'
         return s
 
     def _print_vector(self, vec):
         s = '#('
-        at_start = True
         for e in vec:
-            if at_start:
-                at_start = False
-            else:
+            if s != '#(':
                 s += ' '
-            if e in self._shared:
-                n = self._labels.get(e)
-                if n is None:
-                    n = self._assign_label(e)
-                    s += f'#{n}='
-                    s += self._print(e)
-                else:
-                    s += f'#{n}#'
-            else:
-                s += self._print(e)
+
+            s += self._print(e)
 
         s += ')'
 
