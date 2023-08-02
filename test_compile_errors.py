cf74c4f1aae013dbe50579d2563a5beb9d6dcb7a
Mostafa Razavi <mostafa@sepent.com>
2023-08-02 20:54:50 +0200

    Improve compiler error tests

--- test_compile_errors.py
+++ test_compile_errors.py
@@ -5,22 +5,26 @@ from compile import CompileError, Compiler
 from read import Reader
 
 class TestReader(unittest.TestCase):
-    def _test_toplevel_error(self, source):
+    def _test_toplevel_error(self, source, error=None):
         c = Compiler([])
-        with self.assertRaises(CompileError):
+        with self.assertRaises(CompileError) as e:
             c.compile_toplevel(source)
+        if error:
+            self.assertIn(error, str(e.exception))
 
     def _test_toplevel_noerror(self, source):
         c = Compiler([])
         c.compile_toplevel(source)
 
-    def _test_expr_error(self, source):
+    def _test_expr_error(self, source, error=None):
         c = Compiler([])
         file = io.StringIO(source)
         r = Reader(file)
         expr = r.read()
-        with self.assertRaises(CompileError):
+        with self.assertRaises(CompileError) as e:
             c.compile_form(expr, [])
+        if error:
+            self.assertIn(error, str(e.exception))
 
     def _test_expr_noerror(self, source):
         c = Compiler([])
@@ -33,14 +37,15 @@ class TestReader(unittest.TestCase):
         self._test_toplevel_noerror('(begin)')
 
     def test_empty_expr_begin(self):
-        self._test_expr_error('(begin)')
+        self._test_expr_error('(begin)',
+                              error='Empty "begin" expression')
 
     def test_empty_body_after_defines(self):
         self._test_toplevel_error('''
         (let ()
           (define x 100)
           (define x 200))
-        ''')
+        ''', error='Empty body')
 
     def test_define_in_body(self):
         self._test_toplevel_error('''
@@ -51,4 +56,4 @@ class TestReader(unittest.TestCase):
             (display "foo"))
           (define w 400)
           (list x y z w))
-        ''')
+        ''', error='Ill-placed define')
